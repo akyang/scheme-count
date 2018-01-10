4d5620609d4e540afaa136b6c7fa349e31e8607b
Alex Yang <yangalex@berkeley.edu>
2018-01-10 14:04:27 -0800

    Add tokenizer

--- js/jispy.js
+++ js/jispy.js
@@ -1,3 +1,26 @@
-// Scheme interpreter
+// Scheme Interpreter in Javascript
 
+String.prototype.trim = function () {
+	return this.replace(/^\s+|\s+$/g, '');
+}
 
+// var parse = function (input) {
+// 	return read_from_tokens(tokenize(input));
+// }
+
+// var read_from_tokens = function () {
+// 	if ()
+// }
+
+var tokenize = function (input) {
+	input = input.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ');
+	return input.trim().split(/\s+/g);
+}
+
+// var repl = function (input) {
+// 	try {
+
+// 	} catch (e) {
+
+// 	}
+// }
