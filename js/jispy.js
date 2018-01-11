e3259ebb45d82304a0c6fd29e2e1452d6f2d306c
Alex Yang <yangalex@berkeley.edu>
2018-01-10 16:24:28 -0800

    Complete parser basics

--- js/jispy.js
+++ js/jispy.js
@@ -1,20 +1,65 @@
-// Scheme Interpreter in Javascript
+/** 
+ * Scheme Interpreter in Javascript
+ * Based on Peter Norvig's Lispy scheme interpreter
+ */
 
 String.prototype.trim = function () {
 	return this.replace(/^\s+|\s+$/g, '');
+};
+
+function parse(input) {
+	var tokens = tokenize(input);
+	var expressions = [];
+	while (tokens.length) {
+		var expression = [tokens.shift()];
+		var nests = 0;
+		while (tokens[0] !== ")" || nests !== 0) {
+			var token = tokens.shift();
+			if (tokens.length === 0) {
+				throw new SyntaxError("unexpected EOF missing )");
+			}
+			if (token === "(") {
+				nests++;
+			} else if (token === ")") {
+				nests--;
+			}
+			expression.push(token);
+		}
+		expression.push(tokens.shift());    // pushes last parentheses
+		expressions.push(read_from_tokens(expression));
+	}
+	return expressions;
 }
 
-// var parse = function (input) {
-// 	return read_from_tokens(tokenize(input));
-// }
+function tokenize(input) {
+	return input.replace(/\(/g, " ( ").replace(/\)/g, " ) ").trim().split(/\s+/g);
+}
 
-// var read_from_tokens = function () {
-// 	if ()
-// }
+function read_from_tokens(tokens) {
+	if (tokens.length === 0) {
+		throw new SyntaxError("unexpected EOF while reading");
+	}
+	var token = tokens.shift();
+	if (token === "(") {
+		var L = [];
+		while (tokens[0] != ")") {
+			L.push(read_from_tokens(tokens));
+		}
+		tokens.shift();    // pop off ")"
+		return L;
+	} else if (token === ")") {
+	    throw new SyntaxError('unexpected )');
+	} else {
+		return atom(token);
+	}
+}
 
-var tokenize = function (input) {
-	input = input.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ');
-	return input.trim().split(/\s+/g);
+function atom(token) {
+	if (isNaN(token)) {
+		return token;
+	} else {
+		return +token;
+	}
 }
 
 // var repl = function (input) {
